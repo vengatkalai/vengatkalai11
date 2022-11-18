74766dd11a6e52d790a82b830f765361a9960307
vengatkalai <kalai2942000@gmail.com>
2022-11-18 21:13:40 +0530

    new

---
+++ p1.js
@@ -0,0 +1,27 @@
+var eb =103;
+var output="EB bill your payment";
+var unit=100;
+var punit=3;
+var z,y;
+
+
+if(eb <=unit){
+    console.log("free");
+}
+else{
+    z=eb-unit;
+    y=z*punit;
+    console.log(output+" rupees "+y);
+}
+
+
+
+
+
+
+
+
+
+
+
+
